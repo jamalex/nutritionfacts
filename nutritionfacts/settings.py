73d8c3ab174b403cfabaf2069f8d0f032ff05b48
Aron Fyodor Asor <aronasorman@gmail.com>
2017-11-30 10:48:14 -0800

    add our postgres configuration

--- nutritionfacts/settings.py
+++ nutritionfacts/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'nutritionfacts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME") or "nutritionfacts",
+        'USER': os.getenv("DB_USER") or "nutritionfacts",
+        'PASSWORD': os.getenv("DB_PASSWORD") or "",
+        'HOST': os.getenv("DB_HOST") or "127.0.0.1",
+        'PORT': os.getenv("DB_PORT") or "5432",
     }
 }
 
